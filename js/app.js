de64469cf61f42ee531223c0246f6e920b7f480b
djones1117 <socjones21@yahoo.com>
2023-10-03 10:23:39 -0400

    removing

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
-const greeting = 'hello world'
+// const greeting = 'hello world'
 
-console.log(greeting)
+// console.log(greeting)
+
+// greet("Dylan");
+
+
+// function greet(name) {
+//   console.log("hello , " + name + "!");
+// }
+
+// function sayHello() {
+//     return 'hello!';
+// }
+// console.log(sayHello())
+
+// let number = 1;
+// function addOne(number) {
+// return number + 1;
+// }
+// console.log(addOne(number))
+
+// let a = 1;
+// let b = 2;
+// let sum = a + b;
+// function addTwoNumbers(a,b) {
+// return sum;
+// }
+// console.log(sum)
+
